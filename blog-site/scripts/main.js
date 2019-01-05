aafa3a378c1b9cf2d06efd4bea5112c571ec8cd1
laurensdekloet <46404489+laurensdekloet@users.noreply.github.com>
2019-01-05 10:23:39 -0800

    Add files via upload

---
+++ blog-site/scripts/main.js
@@ -0,0 +1,9 @@
+var myHeading = document.querySelector('h2');
+myHeading.textContent = 'Hello world!';
+
+function multiply(num1,num2) {
+    var result = num1 * num2;
+    alert (result);
+  }
+
+<img src="https://i.imgur.com/7KpCS0Y.jpg" onclick="window.open(this.src)"></img>
